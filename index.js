4e6b2224721987da6987e7a23e1902a754a206b6
Nikolos1987 <85107815+Nikolos1987@users.noreply.github.com>
2022-08-06 15:44:10 +0700

    final

---
+++ index.js
@@ -0,0 +1,34 @@
+var colors = require('colors');
+let n = 100;
+let arr=[]
+nextPrime:
+for (let i = 2; i <= n; i++) { 
+
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue nextPrime; 
+  }
+
+arr.push(i)
+}
+let a=0
+let b=3
+for (i=0;i<Math.ceil(arr.length/3);i++){
+  let arrs=arr.slice(a,b)
+for(j=0;j<arrs.length;j++){
+  if (j==0){
+    console.log(colors.green(arrs[j]))
+  }
+  if (j==1){
+    console.log(colors.yellow(arrs[j]))
+  }
+  if (j==2){
+    console.log(colors.red(arrs[j]))
+  }
+
+
+}
+a=a+3;
+b=b+3
+
+}
+
