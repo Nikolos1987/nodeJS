5e3000923a15876d887b7184e2a94708b817f07f
Nikolos1987 <85107815+Nikolos1987@users.noreply.github.com>
2022-08-11 22:22:24 +0700

    2

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
+const fs = require("fs");
 const colors = require("colors");
 const readline = require("readline");
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+const { Transform } = require("stream");
+const ACCESS_LOG = "./access.log";
+const generateRundomIP = () => {
+  const requests = [
+    `${Math.floor(Math.random() * (999 - 100 + 1)) + 100}.${Math.floor(Math.random() * 10)}.${Math.floor(Math.random() * 10)}.${Math.floor(Math.random() * 10)} - - [25/May/2021:00:07:17 +0000] "GET /foo HTTP/1.1" 200 0 "-" "curl/7.47.0"`,
+  ];
+  fs.appendFile(ACCESS_LOG, requests[0] + "\n", { encoding: "utf8" }, (err) => {
+    if (err) console.log(err);
+  });
+};
 
-rl.question("введите число? ", (answer) => {
+// for (i=0;i<10000;i++){
+//   generateRundomIP()
+// }///запустить для генерации файла 1 мб
+
+// const payedAccount = true;
+
+// const readStream = fs.createReadStream(ACCESS_LOG);
+
+// const tStream = new Transform({
+//   transform(chunk, enconfig, callback) {
+//     if (!payedAccount) {
+//       const transformed = chunk
+//         .toString()
+//         .replace(/\d+\.\d+\.\d+\.\d+/g, "[Hidden IP]");
+//       this.push(transformed);
+//     } else {
+//       this.push(chunk);
+//     }
+
+//     callback();
+//   },
+// });
+// readStream.pipe(tStream).pipe(process.stdout);
+const start=(answer)=>{
+  fs.readFile(ACCESS_LOG, "utf8", 
+  function(error,data){
+      console.log(data);
+      if(error) throw error; 
+      console.log(data);  
+});
+}
+rl.question("введите ip адес ", (answer) => {
   start(answer);
   rl.close();
 });
-
-let start = (answer) => {
-  let n = answer;
-  let arr = [];
-  nextPrime: for (let i = 2; i <= n; i++) {
-    for (let j = 2; j < i; j++) {
-      if (i % j == 0) continue nextPrime;
-    }
-
-    arr.push(i);
-  }
-  let a = 0;
-  let b = 3;
-  for (i = 0; i < Math.ceil(arr.length / 3); i++) {
-    let arrs = arr.slice(a, b);
-    for (j = 0; j < arrs.length; j++) {
-      if (j == 0) {
-        console.log(colors.green(arrs[j]));
-      }
-      if (j == 1) {
-        console.log(colors.yellow(arrs[j]));
-      }
-      if (j == 2) {
-        console.log(colors.red(arrs[j]));
-      }
-    }
-    a = a + 3;
-    b = b + 3;
-  }
-};
